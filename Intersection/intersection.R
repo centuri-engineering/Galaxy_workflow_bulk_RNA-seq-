d110ebe3e4c7a036586b9e4c13c9232209f3a46a
Thomas Vannier <70339871+metavannier@users.noreply.github.com>
2025-07-10 14:20:27 +0200

    Update intersection.R
    Fixe the problemwith the paramter names to have the posibility to select the condition when runing the workflow on galaxy.

--- Intersection/intersection.R
+++ Intersection/intersection.R
@@ -11,6 +11,9 @@ loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
 # Take in trailing command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 
+# Debug: Print all received arguments
+write(paste("Arguments reçus:", paste(args, collapse=", ")), file="debug_args.log")
+
 # Function to get the value of an option from command line arguments
 getOptionValue <- function(option) {
   idx <- match(option, args)
@@ -20,7 +23,7 @@ getOptionValue <- function(option) {
   return(NULL)
 }
 
-# Input
+# Input files
 input_files_up_all <- getOptionValue("--galaxy_input_files_1")
 input_files_down_all <- getOptionValue("--galaxy_input_files_2")
 input_files_stat_all <- getOptionValue("--galaxy_input_files_3")
@@ -28,17 +31,30 @@ name_file_all_genes_stats <- getOptionValue("--galaxy_param1")
 name_file_down <- getOptionValue("--galaxy_param2")
 name_file_up <- getOptionValue("--galaxy_param3")
 coldata <- getOptionValue("--galaxy_param4")
-input_file_1_ref <- getOptionValue("--galaxy_input1")
-input_file_1_mutant <- getOptionValue("--galaxy_input2")
-input_up_or_down_1 <- getOptionValue("--galaxy_input3")
-input_file_2_ref <- getOptionValue("--galaxy_input4")
-input_file_2_mutant <- getOptionValue("--galaxy_input5")
-input_up_or_down_2 <- getOptionValue("--galaxy_input6")
 
+# Parameters from sections (modified names)
+comp1_ref <- getOptionValue("--galaxy_input1")         # Ancien: input_file_1_ref
+comp1_mutant <- getOptionValue("--galaxy_input2")      # Ancien: input_file_1_mutant 
+comp1_regulation <- getOptionValue("--galaxy_input3")  # Ancien: input_up_or_down_1
+comp2_ref <- getOptionValue("--galaxy_input4")         # Ancien: input_file_2_ref
+comp2_mutant <- getOptionValue("--galaxy_input5")      # Ancien: input_file_2_mutant
+comp2_regulation <- getOptionValue("--galaxy_input6")  # Ancien: input_up_or_down_2
 
 output_path1 <- getOptionValue("--output_path1")
 output_path2 <- getOptionValue("--output_path2")
 
+# Debug: Print parameter values
+write(paste(
+  "Paramètres reçus:",
+  "\nComp1 - Ref:", comp1_ref,
+  "\nComp1 - Mutant:", comp1_mutant,
+  "\nComp1 - Regulation:", comp1_regulation,
+  "\nComp2 - Ref:", comp2_ref,
+  "\nComp2 - Mutant:", comp2_mutant,
+  "\nComp2 - Regulation:", comp2_regulation
+), file="debug_params.log")
+
+# Create output directories if they don't exist
 if (!dir.exists(output_path1)) {
   dir.create(output_path1, recursive = TRUE)
 }
@@ -47,6 +63,7 @@ if (!dir.exists(output_path2)) {
   dir.create(output_path2, recursive = TRUE)
 }
 
+# Process input file lists
 input_files_up <- strsplit(input_files_up_all, ",")[[1]]
 input_files_down <- strsplit(input_files_down_all, ",")[[1]]
 input_files_stat <- strsplit(input_files_stat_all, ",")[[1]]
@@ -82,127 +99,119 @@ tryCatch({
   print(paste("Error for STAT matrix:", e$message))
 })
 
+# First comparison
+name_input_file_1 <- paste0(comp1_ref, "_vs_", comp1_mutant, "_signif_", comp1_regulation, "_regulated.txt")
 
-variables <- c("input_file_1_ref", "input_file_1_mutant", "input_up_or_down_1","input_file_2_ref", "input_file_2_mutant", "input_up_or_down_2")
-
-# Verification
-if (all(sapply(variables, exists))) {
-  
-  # Fist comparaison
-
-  #name_input_file_1 <- paste0(input_file_1_ref, "_vs_", input_file_1_mutant,"_signif_",input_up_or_down, "_regulated.txt")
-  name_input_file_1 <- paste0(input_file_1_ref, "_vs_", input_file_1_mutant,"_signif_",input_up_or_down_1,"_regulated.txt")
-
-  if (name_input_file_1 %in% up_matrix[, 1]) {
-    index_file_1 <- which(up_matrix[, 1] == name_input_file_1)
-    input_file_1 <- normalizePath(up_matrix[index_file_1, 2])
-  }
-
-  if (name_input_file_1 %in% down_matrix[, 1]) {
-    index_file_1 <- which(down_matrix[, 1] == name_input_file_1)
-    input_file_1 <- normalizePath(down_matrix[index_file_1, 2])
-  }
-
-  name_input_stat_file_1 <- paste0(input_file_1_ref, "_vs_", input_file_1_mutant, "_all_gene_stats.tsv")
-
-  if (name_input_stat_file_1 %in% stat_matrix[, 1]) {
-    index_stat_file_1 <- which(stat_matrix[, 1] == name_input_stat_file_1)
-    stat_input_file_1 <- normalizePath(stat_matrix[index_stat_file_1, 2])
-  }
-
-  # Second comparaison
-
-  #name_input_file_2 <- paste0(input_file_2_ref, "_vs_", input_file_2_mutant,"_signif_",input_up_or_down, "_regulated.txt")
-  name_input_file_2 <- paste0(input_file_2_ref, "_vs_", input_file_2_mutant,"_signif_",input_up_or_down_2,"_regulated.txt")
-
-  if (name_input_file_2 %in% up_matrix[, 1]) {
-    index_file_2 <- which(up_matrix[, 1] == name_input_file_2)
-    input_file_2 <- normalizePath(up_matrix[index_file_2, 2])
-  }
-
-  if (name_input_file_2 %in% down_matrix[, 1]) {
-    index_file_2 <- which(down_matrix[, 1] == name_input_file_2)
-    input_file_2 <- normalizePath(down_matrix[index_file_2, 2])
-  }
-
-  name_input_stat_file_2 <- paste0(input_file_2_ref, "_vs_", input_file_2_mutant, "_all_gene_stats.tsv")
-
-  if (name_input_stat_file_2 %in% stat_matrix[, 1]) {
-    index_stat_file_2 <- which(stat_matrix[, 1] == name_input_stat_file_2)
-    stat_input_file_2 <- normalizePath(stat_matrix[index_stat_file_2, 2])
-  }
-
-  file_1 = gsub("^.*/", "", name_input_file_1)
-  file_2 = gsub("^.*/", "", name_input_file_2)
-
-  gene_list_1 = read.delim(file = input_file_1,
-                            header = FALSE,
-                            stringsAsFactors = FALSE)
-                
-  gene_list_2 = read.delim(file = input_file_2,
-                            header = FALSE,
-                            stringsAsFactors = FALSE)
-
-
-  ## Data frame with common genes and corresponding fold change
-  # Remove the second column (basemean)
-  gene_list_1 = gene_list_1[,-2]
-  gene_list_2 = gene_list_2[,-2]
-
-  gene_list_1$foldchange_2=gene_list_2[,2][match(gene_list_1[,1], gene_list_2[,1])]
-  gene_list_1$pvalue_2=gene_list_2[,5][match(gene_list_1[,1], gene_list_2[,1])]
-  gene_list_1[,3]=gene_list_1[,5]
-  gene_list_1[,4]=gene_list_1$foldchange_2
-  gene_list_1[,5]=gene_list_1$pvalue_2
-
-  gene_list=gene_list_1[!is.na(gene_list_1[,4]),]
-  gene_list <- gene_list[-1,]
-  colnames(gene_list)[1] = "gene_id"
-  colnames(gene_list)[2] = paste("log2foldchange",file_1,sep = " ")
-  colnames(gene_list)[3] = paste("padj",file_1,sep = " ")
-  colnames(gene_list)[4] = paste("log2foldchange",file_2,sep = " ")
-  colnames(gene_list)[5] = paste("padj",file_2,sep = " ")
-
-  name1 <- paste0("common_genes", file_1, "_vs_", file_2)
-  output_file1 <- file.path(output_path1, paste0(name1, ".txt"))
-
-  write.table(gene_list[,1:5], file = output_file1,row.names=FALSE,col.names = TRUE,sep="\t", quote = FALSE)
-
-  ## Foldgene of the common genes in different selected comparison
-
-  stat_file_1 = gsub("^.*/", "", name_input_stat_file_1)
-  stat_file_2 = gsub("^.*/", "", name_input_stat_file_2)
-
-  stat_1 = read.delim(file = stat_input_file_1,
-                            header = FALSE,
-                            stringsAsFactors = FALSE)
-                            
-  stat_2 = read.delim(file = stat_input_file_2,
-                            header = FALSE,
-                            stringsAsFactors = FALSE)
-
-  stat_1 = stat_1[,-2]
-  stat_2 = stat_2[,-2]
-
-  gene_list$foldchange_stat_1=stat_1[,2][match(gene_list$gene_id, stat_1[,1])]
-  gene_list$pvalue_1=stat_1[,5][match(gene_list$gene_id, stat_1[,1])]
-
-  gene_list$foldchange_stat_2=stat_2[,2][match(gene_list$gene_id, stat_2[,1])]
-  gene_list$pvalue_2=stat_2[,5][match(gene_list$gene_id, stat_2[,1])]
-
-  gene_list[,2]=gene_list$foldchange_stat_1
-  gene_list[,3]=gene_list$pvalue_1
-  gene_list[,4]=gene_list$foldchange_stat_2
-  gene_list[,5]=gene_list$pvalue_2
-
-  colnames(gene_list)[2] = paste("log2foldchange",stat_file_1,sep = " ")
-  colnames(gene_list)[3] = paste("padj",stat_file_1,sep = " ")
-  colnames(gene_list)[4] = paste("log2foldchange",stat_file_2,sep = " ")
-  colnames(gene_list)[5] = paste("padj",stat_file_2,sep = " ")
-
-  name2 <- paste0("stat_common_genes", file_1, "_vs_", file_2)
-  output_file2 <- file.path(output_path2, paste0(name2, ".txt"))
-
-  write.table(gene_list[,1:5], file = output_file2,row.names=FALSE,col.names = TRUE,sep="\t", quote = FALSE)
-
+if (name_input_file_1 %in% up_matrix[, 1]) {
+  index_file_1 <- which(up_matrix[, 1] == name_input_file_1)
+  input_file_1 <- normalizePath(up_matrix[index_file_1, 2])
+} else if (name_input_file_1 %in% down_matrix[, 1]) {
+  index_file_1 <- which(down_matrix[, 1] == name_input_file_1)
+  input_file_1 <- normalizePath(down_matrix[index_file_1, 2])
+} else {
+  stop(paste("Fichier", name_input_file_1, "non trouvé dans les matrices up/down"))
 }
+
+name_input_stat_file_1 <- paste0(comp1_ref, "_vs_", comp1_mutant, "_all_gene_stats.tsv")
+if (name_input_stat_file_1 %in% stat_matrix[, 1]) {
+  index_stat_file_1 <- which(stat_matrix[, 1] == name_input_stat_file_1)
+  stat_input_file_1 <- normalizePath(stat_matrix[index_stat_file_1, 2])
+} else {
+  stop(paste("Fichier stats", name_input_stat_file_1, "non trouvé"))
+}
+
+# Second comparison  
+name_input_file_2 <- paste0(comp2_ref, "_vs_", comp2_mutant, "_signif_", comp2_regulation, "_regulated.txt")
+
+if (name_input_file_2 %in% up_matrix[, 1]) {
+  index_file_2 <- which(up_matrix[, 1] == name_input_file_2)
+  input_file_2 <- normalizePath(up_matrix[index_file_2, 2])
+} else if (name_input_file_2 %in% down_matrix[, 1]) {
+  index_file_2 <- which(down_matrix[, 1] == name_input_file_2)
+  input_file_2 <- normalizePath(down_matrix[index_file_2, 2])
+} else {
+  stop(paste("Fichier", name_input_file_2, "non trouvé dans les matrices up/down"))
+}
+
+name_input_stat_file_2 <- paste0(comp2_ref, "_vs_", comp2_mutant, "_all_gene_stats.tsv")
+if (name_input_stat_file_2 %in% stat_matrix[, 1]) {
+  index_stat_file_2 <- which(stat_matrix[, 1] == name_input_stat_file_2)
+  stat_input_file_2 <- normalizePath(stat_matrix[index_stat_file_2, 2])
+} else {
+  stop(paste("Fichier stats", name_input_stat_file_2, "non trouvé"))
+}
+
+# Process gene lists
+file_1 = basename(name_input_file_1)
+file_2 = basename(name_input_file_2)
+
+gene_list_1 = read.delim(file = input_file_1,
+                         header = FALSE,
+                         stringsAsFactors = FALSE)
+
+gene_list_2 = read.delim(file = input_file_2,
+                         header = FALSE,
+                         stringsAsFactors = FALSE)
+
+## Data frame with common genes and corresponding fold change
+gene_list_1 = gene_list_1[,-2]  # Remove the second column (basemean)
+gene_list_2 = gene_list_2[,-2]
+
+gene_list_1$foldchange_2 = gene_list_2[,2][match(gene_list_1[,1], gene_list_2[,1])]
+gene_list_1$pvalue_2 = gene_list_2[,5][match(gene_list_1[,1], gene_list_2[,1])]
+gene_list_1[,3] = gene_list_1[,5]
+gene_list_1[,4] = gene_list_1$foldchange_2
+gene_list_1[,5] = gene_list_1$pvalue_2
+
+gene_list = gene_list_1[!is.na(gene_list_1[,4]),]
+gene_list <- gene_list[-1,]
+colnames(gene_list)[1] = "gene_id"
+colnames(gene_list)[2] = paste("log2foldchange", file_1, sep = " ")
+colnames(gene_list)[3] = paste("padj", file_1, sep = " ")
+colnames(gene_list)[4] = paste("log2foldchange", file_2, sep = " ")
+colnames(gene_list)[5] = paste("padj", file_2, sep = " ")
+
+# Write common genes output
+name1 <- paste0("common_genes_", comp1_ref, "_vs_", comp1_mutant, "_", comp1_regulation, 
+                "_vs_", comp2_ref, "_vs_", comp2_mutant, "_", comp2_regulation)
+output_file1 <- file.path(output_path1, paste0(name1, ".txt"))
+write.table(gene_list[,1:5], file = output_file1, row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
+
+# Process statistics files
+stat_file_1 = basename(name_input_stat_file_1)
+stat_file_2 = basename(name_input_stat_file_2)
+
+stat_1 = read.delim(file = stat_input_file_1,
+                    header = FALSE,
+                    stringsAsFactors = FALSE)
+
+stat_2 = read.delim(file = stat_input_file_2,
+                    header = FALSE,
+                    stringsAsFactors = FALSE)
+
+stat_1 = stat_1[,-2]
+stat_2 = stat_2[,-2]
+
+gene_list$foldchange_stat_1 = stat_1[,2][match(gene_list$gene_id, stat_1[,1])]
+gene_list$pvalue_1 = stat_1[,5][match(gene_list$gene_id, stat_1[,1])]
+gene_list$foldchange_stat_2 = stat_2[,2][match(gene_list$gene_id, stat_2[,1])]
+gene_list$pvalue_2 = stat_2[,5][match(gene_list$gene_id, stat_2[,1])]
+
+gene_list[,2] = gene_list$foldchange_stat_1
+gene_list[,3] = gene_list$pvalue_1
+gene_list[,4] = gene_list$foldchange_stat_2
+gene_list[,5] = gene_list$pvalue_2
+
+colnames(gene_list)[2] = paste("log2foldchange", stat_file_1, sep = " ")
+colnames(gene_list)[3] = paste("padj", stat_file_1, sep = " ")
+colnames(gene_list)[4] = paste("log2foldchange", stat_file_2, sep = " ")
+colnames(gene_list)[5] = paste("padj", stat_file_2, sep = " ")
+
+# Write statistics output
+name2 <- paste0("stat_common_genes_", comp1_ref, "_vs_", comp1_mutant, "_", comp1_regulation,
+                "_vs_", comp2_ref, "_vs_", comp2_mutant, "_", comp2_regulation)
+output_file2 <- file.path(output_path2, paste0(name2, ".txt"))
+write.table(gene_list[,1:5], file = output_file2, row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
+
+# Debug: Final confirmation
+write("Traitement terminé avec succès", file="debug_completion.log")
